4d94f03f1c6f8fbe4786790ebd1203af2bdbd027
nachi2083 <nachi2083@gmail.com>
2020-07-14 19:42:00 -0300

    Se agregó documentación

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,9 +11,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Configuración de rutas
-app.use(require('./routes/patente.route'));
+// Configuracion de ruta estatica
+app.use(express.static(path.join(__dirname, '../public')));
 
+// Configuración global de las rutas
+app.use(require('./routes/routes'));
+
+// Coneccion a base de datos
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true },
     (err, res) => {
 
@@ -22,6 +27,7 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology:
 
     });
 
+// Inicio y escucha del servidor
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puesto: ' + process.env.PORT);
 });
