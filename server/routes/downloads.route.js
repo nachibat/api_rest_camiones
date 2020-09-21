29200a4e9c0ef462a1c380dae703712439aaaa4f
nachi2083 <nachi2083@gmail.com>
2020-09-21 16:33:10 -0300

    testeos

--- server/routes/downloads.route.js
+++ server/routes/downloads.route.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/download', (req, res) => {
 
-    res.download(path.join(__dirname, '../downloads/agenda--beta-v0.2.0.apk'), 'agenda--beta-v0.2.0.apk');
+    res.download(path.join(__dirname, '../downloads/agenda.v1.0.0.apk'), 'agenda.v1.0.0.apk');
 
 });
 
